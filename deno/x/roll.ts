5f79338c4807b55fe616b4ae7e0b16cc4313fc58
chhayawagmi <52634040+chhayawagmi@users.noreply.github.com>
2023-12-07 12:58:22 +0530

    Create roll.ts

---
+++ deno/x/roll.ts
@@ -0,0 +1,14 @@
+export const functions = {
+  hello: () => {
+    console.log("Hello!");
+  },
+  bye: () => {
+    console.log("Bye!");
+  },
+    name: () => {
+    console.log("User1");
+  },
+    city: () => {
+    console.log("City1");
+  },
+};
