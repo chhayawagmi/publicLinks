46ccd79e2a7d6ddc37836c47cc591ee411647926
chhayawagmi <52634040+chhayawagmi@users.noreply.github.com>
2023-12-07 12:23:37 +0530

    Update index.ts

--- deno/x/index.ts
+++ deno/x/index.ts
@@ -1,3 +1,15 @@
-export function greet() {
-  console.log("Hello, Pluralsight!"); 
+export function hello() {
+  console.log("Hello!"); 
+}
+
+export function bye() {
+  console.log("Bye!"); 
+}
+
+export function address() {
+  console.log("Address"); 
+}
+
+export function name() {
+  console.log("User1"); 
 }
